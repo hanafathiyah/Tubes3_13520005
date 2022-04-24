825475a811c0fb83afa17259f126242f0dda19fd
= <christinehutabarat12@gmail.com>
2022-04-24 22:20:54 +0700

    add insert prediksi function

--- src/main.js
+++ src/main.js
@@ -20,3 +20,125 @@ function levenshtein(stra, strb) {
         return 1 + Math.min(levenshtein(tail_a, strb), levenshtein(stra, tail_b), levenshtein(tail_a, tail_b));
     }
 }
+
+// untuk sementara taro sini dulu fungsi border sama kmp, soalnya belum tau cara import di js
+function border(text) {
+    const b = []; // array of sizes of the same prefix and suffix
+    b[0] = 0;
+    var j; // mismatch position
+    var k; // position before mismatch
+    for (j = 2; j < text.length; j++) {
+        k = j - 1;
+        
+        var suffix = "";
+        var size = 0;
+        var match = true;
+        var n = 1;
+
+        while (match && n <= k) {
+            var prefix = "";
+            var suffix = "";
+            var i = 0;
+            //console.log(k, n)
+            while (i < n) {
+                prefix += text[i];
+                i++;
+            }
+            //console.log("prefix :", prefix);
+            i = k - n + 1;
+            while (i <= k) {
+                suffix += text[i];
+                i++;
+            }
+            //console.log("suffix :", suffix);
+            if (prefix == suffix && n > size) {
+                size = n;
+                match = true;
+            }
+            else {
+                n++;
+            }
+        }
+        b[k] = size;
+    }
+    return b;
+}
+
+function kmpMatch(text, pattern) {
+    var match = false;
+    var t_length = text.length;
+    var p_length = pattern.length;
+
+    var fail = border(pattern);
+
+    var i = 0; // iterator for text
+    var j = 0; // iterator for pattern
+
+    while (!match && i < t_length) {
+        if (text[i] == pattern[j]) {
+            if (j == p_length - 1) {
+                match = true;
+            }
+            else {
+                i++;
+                j++;
+            }
+        }
+        else {
+            if (j == 0) {
+                i++;
+                j = 0;
+            }
+            else {
+                j = fail[j-1];
+            }
+        }
+    }
+    return match;
+}
+
+function predict(name, dna_sequence, disease) {
+    var select_query = "SELECT rantai FROM penyakit WHERE nama_penyakit = \'" + disease + "\'";
+    var insert_query = "INSERT INTO prediksi (tanggal, nama_pasien, penyakit, status) VALUES (\'";
+    var d = new Date();
+    var day = d.getDate();
+    var month = d.getMonth();
+    var year = d.getFullYear();
+    insert_query += year + "-" + month + "-" + day + "\',\'";
+    insert_query += name + "\',\'";
+    insert_query += disease + "\',\'";
+    var pattern;
+    var res;
+
+    con.query(select_query, function(err, result) {
+        if (err) throw err;
+        pattern = result[0].rantai;
+        if (kmpMatch(dna_sequence, pattern)) {
+            res = "True";
+        }
+        else {
+            res = "False";
+        }
+        insert_query += res + "\')";
+        con.query(insert_query, function(err) {
+            if (err) throw err;
+            console.log(year + "-" + month + "-" + day + " - " + name + " - " + disease + " - " + res);
+        });
+    });
+}   
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  database : "tubes3stima",
+  user: "root",
+  password: "root" // ganti password di sini
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  predict("Aleksey Romanov", "GTACAGTTCATAGCGTCAAGTGTACA", "hemophilia");
+  
+});
